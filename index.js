40c63c0f95143de1e13bb2943fb9ca2bf5d707a6
EdPaul1 <saidarajab101@gmail.com>
2023-03-28 15:49:13 +0300

    Big O

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const read = new Set();
+  for (i = 0; i < array.length; i++){
+    const remainder = target - array[i];
+    if (read.has(remainder)){
+      return true;
+    }
+    read.add(array[i]);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
-
+// O(n) - the time complexity is linear in the size of the input array. The function iterates through the input array once and for each element it checks if it is in the set.
 /* 
   Add your pseudocode here
+
 */
 
 /*
   Add written explanation of your solution here
+  The function hasTarget takes in two elements as the input. an array of integers and an integer as a target
+  a variable read is a new set that records elements of the array that the for loop as iterated over.
+  the remainder variable takes in the target element and all numbers in the array are subtracted from the target and returns true if a matching value is in the set.
+  
 */
 
 // You can run `node index.js` to view these console logs
